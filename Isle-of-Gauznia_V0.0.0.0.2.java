23f64ced25d619ebed20026a60a72498092bea6a
Michael Arnold <valestrum@hotmail.com>
2014-02-11 02:06:12 -0500

    Update and rename Isle-of-Gauznia_V0.0.0.0.1.java to Isle-of-Gauznia_V0.0.0.0.2.java
    Deleted useless code, cleaned up code, and added comments.

--- Isle-of-Gauznia_V0.0.0.0.2.java
+++ Isle-of-Gauznia_V0.0.0.0.2.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 
 public class main
 {
-	
+	private static Scanner input;
+
 	public static void main(String[] args)
 	{
-		int health, armor, attack, def, str, magic, ranged;
-		String playerName;
-		
-		Scanner input = new Scanner(System.in);
+
+		int health, armor, attack, def, str, magic, ranged; // Currently unused
+
+		String playerName; 
+		input = new Scanner(System.in);
 		
 		System.out.println("Greetings, and welcome to Isle of Gauznia!\n");
 		System.out.println("What is your name, adventurer? ");
-		playerName = input.next();
+		playerName = input.next(); // Records the user's name.
 		
 		System.out.println("Welcome, " + playerName + "!");
 	}
