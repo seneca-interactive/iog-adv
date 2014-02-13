65ef74908d935690e1c224335a884b3af4629113
clmc <zcray@icloud.com>
2014-02-12 23:06:10 -0500

    arrays

--- src/main.java
+++ src/main.java
@@ -16,5 +16,34 @@ public class main
 		playerName = input.next(); // Records the user's name.
 		
 		System.out.println("Welcome, " + playerName + "!");
+
+
+		System.out.println("An enemy approaches!");
+
+//		int loop_counter = 0;
+//		while (loop_counter > 10){
+//		loop_counter += 1;
+//		System.out.println("We've printed this line " + loop_counter + " times.");
+//		}
+
+//		int round_counter = 0;
+
+
+//   Player Class Arrays
+//	ARRAYS SYNTAX:
+// hp(0), armor(1), attack(2), def(3), str(4), magic(5), ranged(6)
+		int[] assassin_Ary = { 13,2,6,2,3,1,2 };
+		int[] necroman_Ary = { 8,1,3,2,2,7,1 };
+		int[] ranger_Ary   = { 12,3,4,3,3,1,6 };
+
+
+
+//   Enemy Combatants
+//   (Same array syntax as player classes)
+		int[] rat_stats = { 4,1,1,1,1,1,0 };		
+		int[] bat_stats = { 2,1,1,1,1,1,0 };
+
+		System.out.println( assassin_Ary[5]);
+
 	}
 }
