07f5624c3789965a1d6595ca80daa6739b0d7af7
clmc <zcray@icloud.com>
2014-02-14 00:09:35 -0500

    weapons module

---
+++ src/weapons.java
@@ -0,0 +1,31 @@
+// Isle-of-Gauznia
+// weapons module 
+
+public class weapons
+{
+	// min(0), max(1), att(2), str(3), crit(4)
+	// bronze weapons
+	int[] bronze_dirk = { 1,2,1,1,6 };
+	int[] bronze_dagger = { 1,2,2,1,7 };
+	int[] bronze_shortsword = { 2,4,3,2,5 };
+	int[] bronze_scimitar = { 1,5,3,3,3 };
+	int[] bronze_longsword = { 2,5,3,3,3 };
+	int[] bronze_hatchet = { 3,4,2,4,2 };
+	int[] bronze_waraxe = { 2,5,2,4,3 };
+	int[] bronze_battleaxe = { 2,7,2,5,4 };
+
+	// iron weapons
+	int[] iron_dirk = { 1,3,2,1,7 };
+	int[] iron_dagger = { 1,3,2,1,9 };
+	int[] iron_tanto = { 1,4,2,2,9 };
+	int[] iron_katana = { 2,8,7,7,9 };
+
+	// steel weapons
+	int[] steel_stiletto = { 1,3,3,2,12 };
+	int[] steel_dagger = { 2,3,3,2,11 };
+	int[] steel_tanto = { 2,4,3,3,11 };
+	int[] steel_shortsword = { 2,6,4,3,8 };
+	int[] steel_scimitar = { 2,6,5,5,9 };
+	int[] steel_longsword = { 2,6,6,4,10 };
+
+}
