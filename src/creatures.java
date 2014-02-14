d32664acec95c57e2334089b706431af46a66db9
clmc <zcray@icloud.com>
2014-02-14 13:59:53 -0500

    creatures

---
+++ src/creatures.java
@@ -0,0 +1,19 @@
+// Isle-of-Gauznia
+// weapons module 
+
+public class creatures
+{
+	// hostile(0), hp(1), armor(2), attack(3), def(4), str(5),
+	// magic(6), ranged(7)
+
+	int[] creature_bat = { 1,2,1,1,1,1,1,0 };
+	int[] creature_rat = { 1,4,1,1,1,1,1,0 };
+	int[] creature_crab = { 1,7,7,1,7,1,1,0 };
+	int[] creature_kobold = { 1,7,1,2,1,5,1,0 };
+	int[] creature_stuut = { 1,6,5,1,5,1,3,0 }; // giant beetle
+	int[] creature_zombie_lesser = { 1,12,1,5,1,4,0,0 };
+	int[] creature_zombie = { 1,19,1,6,2,6,0,0 };
+	int[] creature_zombie_greater = { 1,57,2,8,4,13,0,0 };
+	int[] creature_tribal_ghost = { 1,17,1,5,2,5,6,0 };
+
+	}
