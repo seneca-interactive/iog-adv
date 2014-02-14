b59fc5c72fc38266dd15a741ac47fe045a227a97
clmc <zcray@icloud.com>
2014-02-14 11:37:18 -0500

    armor_gear

---
+++ src/armor_gear.java
@@ -0,0 +1,42 @@
+// Isle-of-Gauznia
+// armor module 
+
+public class armor_gear
+{
+	// armor(0), def(1), 
+	// copper armor
+	int[] copper_bracers = { 1,0 };	
+	int[] copper_breastplate = { 2,1 };
+	int[] copper_headband = { 1,0 };
+	int[] copper_buckler = { 1,2 };
+
+	// bronze armor
+	int[] bronze_boots = { 3,1 };
+	int[] bronze_bracers = { 1,1 };	
+	int[] bronze_pauldron_left = { 2,1 }; // shield arm pauldron
+	int[] bronze_pauldron_right_sm = { 1,1 }; // small pauldron
+	int[] bronze_breastplate = { 5,2 };
+	int[] bronze_breastplate_runed = { 7,4 };
+	int[] bronze_cuirass = { 8,5 };
+	int[] bronze_helmet_small = { 2,2 };
+	int[] bronze_helmet_full = { 3,3 };
+	int[] bronze_helmet_hoplite = { 4,3 };
+	int[] bronze_buckler = { 5,5 };
+	
+	// iron armor
+	int[] iron_gauntlets = { 3,2 };
+	int[] iron_bracers = { 3,1 };	
+	int[] iron_chainmail = { 6,2 };
+	int[] iron_cuirass = { 11,6 };
+	int[] iron_helmet_small = { 4,3 };
+	int[] iron_helmet_full = { 5,4 };
+	int[] iron_boots = { 5,2 };
+	int[] iron_pauldron_left = { 3,2 }; // shield arm pauldron
+	int[] iron_pauldron_right = { 3,2 }; // shield arm pauldron
+	int[] iron_pauldron_right_sm = { 2,2 }; // small pauldron
+	int[] iron_greaves = { 7,4 };
+	int[] iron_buckler = { 6,6 };
+	int[] iron_shield_circle = { 7,7 };
+	int[] iron_shield_tower = { 8,9 };
+
+}
