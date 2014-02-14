1fe5b43069e06fed23666aaf78815e5f25b14ec7
Michael Arnold <valestrum@hotmail.com>
2014-02-14 18:23:22 -0500

    Update creatures.java

--- src/creatures.java
+++ src/creatures.java
@@ -1,19 +1,35 @@
 // Isle-of-Gauznia
-// weapons module 
+// weapons module .. Oh really now? ;P
+
+// Note: "V." = User: ArnoldM904 content, feel free to edit or delete.
+
+// Creature idea list:
+// - Add potential to have modified creatures 
+// IE: Frenzied, greater, lesser, etc..
+// Where for example you may run into a frenzied bat that has extra att + str occasionally.
 
 public class creatures
 {
 	// hostile(0), hp(1), armor(2), attack(3), def(4), str(5),
 	// magic(6), ranged(7)
 
+	int[] creature_snake = {1,1,1,2,1,1,0,0}; //V.
 	int[] creature_bat = { 1,2,1,1,1,1,1,0 };
 	int[] creature_rat = { 1,4,1,1,1,1,1,0 };
 	int[] creature_crab = { 1,7,7,1,7,1,1,0 };
 	int[] creature_kobold = { 1,7,1,2,1,5,1,0 };
+	int[] creature_goblin = {1,10,1,3,1,3,0,0,}; //V.
 	int[] creature_stuut = { 1,6,5,1,5,1,3,0 }; // giant beetle
 	int[] creature_zombie_lesser = { 1,12,1,5,1,4,0,0 };
 	int[] creature_zombie = { 1,19,1,6,2,6,0,0 };
 	int[] creature_zombie_greater = { 1,57,2,8,4,13,0,0 };
 	int[] creature_tribal_ghost = { 1,17,1,5,2,5,6,0 };
+	int[] creature_ogre = {}; //V.
+	int[] creature_cyclops = {}; //V.
+	int[] creature_minotaur = {}; //V.
+	int[] creature_bandit = {}; //V.
+	int[] creature_marksman = {}; //V.
+	int[] creature_wizard = {}; //V.
+	int[] creature_dark_wizard = {}; //V.
 
 	}
